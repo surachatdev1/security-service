638becf5f22ba4cb478c6c3eb54913b2a9da14f8
surachatdev1 <surachat.dev1@gmail.com>
2020-09-18 00:49:40 +0700

    remove bad import

--- src/main/java/com/nls/service/security/dao/UserDao.java
+++ src/main/java/com/nls/service/security/dao/UserDao.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.rowset.CachedRowSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
